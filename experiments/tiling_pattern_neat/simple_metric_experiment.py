2b554fc60d670273250bb54214d45edbad8062da
Matthijs den Toom <M.denToom@student.tudelft.nl>
2019-01-26 11:41:14 +0100

    Updated first experiment

--- experiments/tiling_pattern_neat/simple_metric_experiment.py
+++ experiments/tiling_pattern_neat/simple_metric_experiment.py
@@ -9,10 +9,13 @@ import time
 import neat
 import gym
 import gym_multi_robot
+from experiments import visualize
+from experiments.genome_serializer import GenomeSerializer
 
 num_steps = 3000
 num_robots = 5
-num_trials = 10
+num_trials = 5
+num_generations = 100
 
 env = gym.make('tiling-pattern-v0')
 
@@ -62,10 +65,24 @@ def run(config_file):
     p.add_reporter(stats)
 
     # Run for up to 300 generations.
-    winner = p.run(eval_genomes, 300)
+    winner = p.run(eval_genomes, num_generations)
 
-    # Display the winning genome.
-    print('\nBest genome:\n{!s}'.format(winner))
+    visualize.visualize_stats(stats)
+    output_winner(winner, config)
+
+
+def output_winner(winner, config, net_filename='nn_winner', genome_filename='winner'):
+    """This function outputs the current winner in graph and in pickle file."""
+
+    node_names = {-1: 'hold', -2: 'on object', -3: '1_obstacle', -4: '1_tile', -5: '1_robot', -6: '2_obstacle',
+                  -7: '2_tile', -8: '2_robot', -9: '3_obstacle', -10: '3_tile', -11: '3_robot', -12: '4_obstacle',
+                  -13: '4_tile', -14: '4_robot', -15: '5_obstacle', -16: '5_tile', -17: '5_robot',
+                  0: 'drive', 1: 'rotation', 2: 'pickup', 3: 'put down'}
+    visualize.draw_net(config, winner, node_names=node_names, filename=net_filename)
+
+    GenomeSerializer.serialize(winner, genome_filename)
+
+    print(winner)
 
 
 if __name__ == '__main__':
