Fixed bug in fitness calculation

gym_multi_robot/envs/tests/test_tiling_pattern_game.py
@@ -2,13 +2,25 @@ import unittest
 
 import numpy as np
 
-from gym_multi_robot.envs.tiling_pattern_game import TilingPatternGame
+from gym_multi_robot.envs.gripping_robot import Heading
+from gym_multi_robot.envs.tiling_pattern_game import TilingPatternGame, StaticTilingPatternGame
+
+
+def static_correct_grid():
+
+    grid = np.zeros((7, 5), dtype=int)
+    for i in range(0, 7, 2):
+        for j in range(0, 5, 2):
+            grid[i][j] = 1
+
+    return grid
 
 
 class TestGripperRobot(unittest.TestCase):
 
     def test_one_by_one_field(self):
         game = TilingPatternGame((1, 1), 1, 0)
+        game.reset()
         self.assertEqual(1, game.num_tiles)
         self.assertTrue(game.grid[0][0])
 
@@ -22,6 +34,7 @@ class TestGripperRobot(unittest.TestCase):
 
     def test_11x11_field(self):
         game = TilingPatternGame((11, 11), 2, 0)
+        game.reset()
         self.assertEqual(36, game.num_tiles)
         self.assertEqual(36, np.sum(np.sum(game.grid)))
 
@@ -43,6 +56,68 @@ class TestGripperRobot(unittest.TestCase):
         self.assertFalse(game.inside_grid((11, 11)))
         self.assertFalse(game.inside_grid((-1, 11)))
         self.assertFalse(game.inside_grid((11, -1)))
+
+    def test_tiling_pattern_fitness(self):
+
+        grid = static_correct_grid()
+        game = StaticTilingPatternGame(grid, 2, [])
+        game.reset()
+
+        self.assertEqual(12, game.num_tiles)
+        self.assertAlmostEqual(100, game.get_fitness())
+
+    def test_static_pattern_reset(self):
+
+        grid = static_correct_grid()
+        game = StaticTilingPatternGame(grid, 2, [])
+        game.reset()
+
+        grid[0][0] = 0
+        grid[0][3] = 1
+
+        self.assertLessEqual(99.5, game.get_fitness())
+        game.reset()
+
+        self.assertEqual(12, game.num_tiles)
+        self.assertAlmostEqual(100, game.get_fitness())
+
+    def test_static_robot_reset(self):
+        grid = static_correct_grid()
+        game = StaticTilingPatternGame(grid, 2, [((0, 0), Heading.NORTH)])
+        game.reset()
+        self.assertEqual((0, 0), game.robots[0].location)
+
+        # Move the robot.
+        game.robots[0].move(1, 1, game)
+        self.assertFalse(game.has_robot((0, 0)))
+
+        # Reset the game and check that robot has original parameters.
+        game.reset()
+        self.assertEqual((0, 0), game.robots[0].location)
+        self.assertEqual(Heading.NORTH, game.robots[0].heading)
+        self.assertTrue(game.has_robot((0, 0)))
+
+    def test_static_robots_reset(self):
+        grid = static_correct_grid()
+        game = StaticTilingPatternGame(grid, 2, [((0, 0), Heading.NORTH), ((3, 3), Heading.SOUTH)])
+        game.reset()
+        self.assertEqual((0, 0), game.robots[0].location)
+        self.assertEqual((3, 3), game.robots[1].location)
+
+        # Move the robot.
+        game.robots[0].move(1, 1, game)
+        game.robots[1].move(1, 1, game)
+        self.assertFalse(game.has_robot((0, 0)))
+        self.assertFalse(game.has_robot((3, 3)))
+
+        # Reset the game and check that robot has original parameters.
+        game.reset()
+        self.assertEqual((0, 0), game.robots[0].location)
+        self.assertEqual((3, 3), game.robots[1].location)
+        self.assertEqual(Heading.NORTH, game.robots[0].heading)
+        self.assertEqual(Heading.SOUTH, game.robots[1].heading)
+        self.assertTrue(game.has_robot((0, 0)))
+        self.assertTrue(game.has_robot((3, 3)))
 
 
 if __name__ == '__main__':

gym_multi_robot/envs/tiling_pattern_game.py
@@ -13,12 +13,10 @@ class TilingPatternGame:
         self.lattice_size = lattice_size
 
         self.grid = np.zeros((self.GRID_W, self.GRID_H), dtype=int)
-        self.num_tiles = int(math.ceil((self.GRID_W) / self.lattice_size) * math.ceil((self.GRID_H) / self.lattice_size))
+        self.num_tiles = int(math.ceil(self.GRID_W / self.lattice_size) * math.ceil(self.GRID_H / self.lattice_size))
 
         self.num_robots = num_robots
         self.robots = []
-
-        self.reset()
 
     def reset_grid(self):
 
@@ -70,7 +68,7 @@ class TilingPatternGame:
         for i in range(0, len(self.grid) - 1, self.lattice_size): # -1 to not take into account the last block.
             for j in range(0, len(self.grid[0]) - 1, self.lattice_size):
 
-                sub_grid = self.grid[i:i + self.lattice_size + 1][j:j + self.lattice_size + 1]
+                sub_grid = self.grid[i:i + self.lattice_size + 1, j:j + self.lattice_size + 1]
                 count = np.sum(np.sum(sub_grid))
                 p_js.append(count)
 
@@ -134,16 +132,16 @@ class StaticTilingPatternGame(TilingPatternGame):
     at the same locations every time.
     """
 
-    def __init__(self, grid_size, lattice_size, grid, robot_pos):
-        super().__init__(grid_size, lattice_size, len(robot_pos))
-        self.default_grid = grid
+    def __init__(self, grid, lattice_size, robot_pos):
+        super().__init__(grid.shape, lattice_size, len(robot_pos))
+        self.default_grid = np.copy(grid)
         self.default_robot_pos = robot_pos  # contains a position at index 0 and a heading at index 1
 
     def reset_grid(self):
-        self.grid = np.array(self.default_grid, copy=True)
+        self.grid = np.copy(self.default_grid)
 
     def reset_robots(self):
-        self.robots = [GripperRobot(i, self.default_robot_pos[0], self.default_robot_pos[1])
-                       for i in range(self.default_robot_pos)]
+        self.robots = [GripperRobot(i, self.default_robot_pos[i][1], self.default_robot_pos[i][0])
+                       for i in range(len(self.default_robot_pos))]
 
         return [robot.get_observation(self) for robot in self.robots]