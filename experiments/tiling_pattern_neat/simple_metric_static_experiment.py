6901275e66add1c0b7ca8e48361940b39e41d6cd
Matthijs den Toom <M.denToom@student.tudelft.nl>
2019-02-02 15:55:16 +0100

    Generalized serializer

--- experiments/tiling_pattern_neat/simple_metric_static_experiment.py
+++ experiments/tiling_pattern_neat/simple_metric_static_experiment.py
@@ -9,7 +9,7 @@ import time
 import neat
 import gym
 from gym_multi_robot import visualize
-from gym_multi_robot.genome_serializer import GenomeSerializer
+from gym_multi_robot.object_serializer import ObjectSerializer
 
 num_steps = 3000
 num_robots = 5
@@ -74,7 +74,7 @@ def output_winner(winner, config, net_filename='nn_winner', genome_filename='win
                   0: 'drive', 1: 'rotation', 2: 'pickup', 3: 'put down'}
     visualize.draw_net(config, winner, node_names=node_names, filename=net_filename)
 
-    GenomeSerializer.serialize(winner, genome_filename)
+    ObjectSerializer.serialize(winner, genome_filename)
 
     print(winner)
 
--- gym_multi_robot/object_serializer.py
+++ gym_multi_robot/object_serializer.py
@@ -1,14 +1,14 @@
 import pickle
 
 
-class GenomeSerializer:
+class ObjectSerializer:
     """This class can write and read genomes, so that they can be used later in the program"""
 
     @classmethod
-    def serialize(cls, genome, file_name):
+    def serialize(cls, object, file_name):
         """This class serializes the given object in the given file name with .pickle behind it."""
         with open(file_name + '.pickle', 'wb') as handle:
-            pickle.dump(genome, handle, protocol=pickle.HIGHEST_PROTOCOL)
+            pickle.dump(object, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
     @classmethod
     def load(cls, file_name):
