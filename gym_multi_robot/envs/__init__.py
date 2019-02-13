2f56eb25d5a41096493296539f116ef0d3ddc3fd
Matthijs den Toom <M.denToom@student.tudelft.nl>
2019-02-13 13:29:11 +0100

    Alternative fitness for tiling pattern game

--- gym_multi_robot/envs/__init__.py
+++ gym_multi_robot/envs/__init__.py
@@ -1,2 +1,3 @@
 from gym_multi_robot.envs.tiling_pattern_env import TilingPatternEnv
+from gym_multi_robot.envs.tiling_pattern_game_alternative import CountTilingPatternEnv
 from gym_multi_robot.envs.foraging_env import ForagingEnv
